67f00eb9ecc234f7ee1fb6b984b0ebdeede68b33
MtinAMD <Mtin.AMD@gmail.com>
2025-11-02 14:04:57 +0330

    Update process.c

--- process.c
+++ process.c
@@ -13,7 +13,10 @@ int main() {
     //int numbers[] = {1,2,3,4,5,67,5,3,78,77,66,55,99,14,34,2,56,56};
     //int n_size = sizeof(numbers)/sizeof(numbers[0]);
 
-    int n_size -  sizeof(numbers)/sizeof(numbers[0]);
+    //int n_size -  sizeof(numbers)/sizeof(numbers[0]);
+
+    clock_t start_time = clock();
+
     if (pipe(pipefd) == -1) {
         perror("pipe failed");
         return 1;
@@ -27,29 +30,33 @@ int main() {
 
     if (pid > 0) {
         // producer
-        //printf("producer started\n");
+        printf("producer started\n");
         close(pipefd[0]); // close reading
         for (int i = 0; i < NUMS; i++) {
             write(pipefd[1], &i, sizeof(i));
         }
         close(pipefd[1]);
         wait(NULL);
-        //printf("producer finished\n");
+        printf("producer finished\n");
 
     } else {    // pid = 0
         // consumer
-        //printf("consumer started\n");
+        printf("consumer started\n");
         close(pipefd[1]); // close writing
 
         int value;
         while (read(pipefd[0], &value, sizeof(value)) > 0) {
             sum += value;
         }
-        //printf("consumer result: sum = %d\n", sum);
+        printf("consumer result: sum = %d\n", sum);
 
         close(pipefd[0]);
         exit(0);
     }
+
+    clock_t end_time = clock();
+    double execution_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    printf("Execution time: %.4f seconds\n", execution_time)
 
     return 0;
 }
