5eeebaa629f7cbaa55e3054e5dce12574bab5c08
MtinAMD <Mtin.AMD@gmail.com>
2025-11-02 13:49:11 +0330

    Initial commit

---
+++ process.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define NUMS 100000
+
+int main() {
+    int pipefd[2];
+    pid_t pid;
+    int sum = 0;
+
+    //int numbers[] = {1,2,3,4,5,67,5,3,78,77,66,55,99,14,34,2,56,56};
+    //int n_size = sizeof(numbers)/sizeof(numbers[0]);
+
+    int n_size -  sizeof(numbers)/sizeof(numbers[0]);
+    if (pipe(pipefd) == -1) {
+        perror("pipe failed");
+        return 1;
+    }
+    
+    pid = fork();
+    if (pid < 0) {
+        perror("fork fail");
+        return 1;
+    }
+
+    if (pid > 0) {
+        // producer
+        //printf("producer started\n");
+        close(pipefd[0]); // close reading
+        for (int i = 0; i < NUMS; i++) {
+            write(pipefd[1], &i, sizeof(i));
+        }
+        close(pipefd[1]);
+        wait(NULL);
+        //printf("producer finished\n");
+
+    } else {    // pid = 0
+        // consumer
+        //printf("consumer started\n");
+        close(pipefd[1]); // close writing
+
+        int value;
+        while (read(pipefd[0], &value, sizeof(value)) > 0) {
+            sum += value;
+        }
+        //printf("consumer result: sum = %d\n", sum);
+
+        close(pipefd[0]);
+        exit(0);
+    }
+
+    return 0;
+}
